6bfc932434dbf1f36e680b8069ea38e3523a5362
Coda Hale <coda.hale@gmail.com>
2017-06-11 19:50:02 -0700

    Added a perf harness.

---
+++ src/test/java/com/codahale/aesgcmsiv/perf/PerfHarness.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codahale.aesgcmsiv.perf;
+
+import com.codahale.aesgcmsiv.AEAD;
+import okio.ByteString;
+
+public class PerfHarness {
+
+  @SuppressWarnings("ResultOfMethodCallIgnored")
+  public static void main(String[] args) {
+    final AEAD aead = new AEAD(ByteString.of(new byte[16]));
+    final ByteString nonce = ByteString.of(new byte[12]);
+    final ByteString plaintext = ByteString.of(new byte[1024]);
+
+    for (int i = 0; i < 1_000_000; i++) {
+      aead.seal(nonce, plaintext, ByteString.EMPTY);
+    }
+  }
+}
